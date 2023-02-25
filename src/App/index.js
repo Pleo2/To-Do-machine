5dbe32d2abbf59482eb9c63eb00b4c37860253a6
pleo <leooel23m@gmail.com>
2023-02-24 21:00:23 -0400

    hot fix: remove todos default local storage

--- src/App/index.js
+++ src/App/index.js
@@ -10,21 +10,3 @@ export default function App() {
     </TodoProvider>
   );
 }
-
-const itemId = () => Math.floor(Math.random() * 1000);
-const DefaultTodos = JSON.stringify([
-  {
-    text: 'Ingles practica en la mananna muajajaja 🟠',
-    completed: false,
-    itemId: itemId(),
-  },
-  {
-    text: 'Platzi termina curso de React 📖',
-    completed: false,
-    itemId: itemId(),
-  },
-  { text: 'Ver the office 📺', completed: true, itemId: itemId() },
-  { text: 'Jugar Valorant 🎮', completed: true, itemId: itemId() },
-  { text: 'Arreglar switch 🏏 ', completed: false, itemId: itemId() },
-]);
-localStorage.setItem('TODOS_V2', DefaultTodos);
